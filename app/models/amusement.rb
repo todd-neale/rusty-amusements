fe650c803c436c4b80f1eb87f384a5abf77cfb2c
Todd <toddneale13@gmail.com>
2021-02-23 15:58:44 +0000

    added categories and more into the amusements model

--- app/models/amusement.rb
+++ app/models/amusement.rb
@@ -2,5 +2,7 @@ class Amusement < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
 
-  validates :name, :description, :price, presence: true
+  validates :name, :tagline, :description, :price, :deathcount, :size, :category, :haskilledanimals, :washingmachine, :childunfriendly, :heightrestriction, :haunting, :illegal,
+  presence: true
 end
+
