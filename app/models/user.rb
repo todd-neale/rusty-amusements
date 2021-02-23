8c95c1e846afdaea920b54e3256577b457308b53
Arthur Campbell <arfa.camble@gmail.com>
2021-02-23 18:02:28 +0000

    user updated with name, update button on profile and update form improved. cloudinary integration for profile photos and displayed in navbar and on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   has_one_attached :photo
   has_many :amusements
+  before_validation :name_or_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+  def name_or_email
+    unless self.name
+      self.name = self.email.split('@').first
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   root to: 'pages#home'
   resources :amusements do
     resources :bookings, only: [:new, :create, :edit, :update]
   end
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
