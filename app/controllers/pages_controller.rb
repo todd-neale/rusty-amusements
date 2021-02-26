83ac11f9f4dd1a81c4b11e1d1de29e514e1eb79b
Alex Marriott <marriottaog@gmail.com>
2021-02-26 12:30:39 +0000

    WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,14 @@ class PagesController < ApplicationController
 
   def home
     @amusements = Amusement.limit(4)
+    @total = 0
+    @count = 0
+    Amusement.all.each do |amusement|
+      amusement.reviews.each do |review|
+        @total += review.rating
+        @count += 1
+      end
+    end
+    @sum = @total/@count
   end
 end
