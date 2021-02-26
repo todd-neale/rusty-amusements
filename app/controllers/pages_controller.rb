8209fb5e3efd83621b23c10abdb5901fd64cc3e6
Alex Marriott <marriottaog@gmail.com>
2021-02-26 13:46:17 +0000

    added logic to front page, now lists only 4 amusements in descending review order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @amusements = Amusement.limit(4)
-    @total = 0
-    @count = 0
-    Amusement.all.each do |amusement|
+    @amusements = Amusement.all.select {|amusement| amusement.reviews != [] }
+    @amusements = @amusements.map do |amusement|
+      total = 0
+      count = 0
       amusement.reviews.each do |review|
-        @total += review.rating
-        @count += 1
+        total += review.rating.to_f
+        count += 1
+      @sum = (total/count).round(1)
       end
+      [amusement, @sum]
     end
-    @sum = @total/@count
+
+    @amusements = @amusements.sort {|a,b| b[1] <=> a[1]}
+    @amusements = @amusements.map {|amusement| amusement[0]}[0..3]
   end
 end
